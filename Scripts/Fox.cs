bfdfe7971e1027798fc605b34d902d2d63df57b9
tom <tom.maxwellmans@gmail.com>
2023-02-04 18:57:43 +1300

    Fox Moves in direction

--- Scripts/Fox.cs
+++ Scripts/Fox.cs
@@ -11,6 +11,7 @@ public class Fox : MonoBehaviour
     private float moveTime = 2;
     private bool moveMode = false;
     private Vector3 moveDirection;
+    private Quaternion newDirection;
     
 
     private Rabbit thisRabbit;
@@ -20,6 +21,9 @@ public class Fox : MonoBehaviour
     {
         moveDirection = new Vector3(Random.Range(-1f, 1f),0f, Random.Range(-1f, 1f));
         moveDirection.Normalize();
+
+        newDirection = Quaternion.Euler(moveDirection);
+        transform.rotation = newDirection;
         moveDirection *= 10f;
 
     }
@@ -44,6 +48,14 @@ public class Fox : MonoBehaviour
             moveDirection.Normalize();
             moveDirection *= 10f;
 
+            // Find the angle from point on a x z plane
+            float radianAngle = Mathf.Atan2(moveDirection.x, moveDirection.z);
+            float angle = 180 * radianAngle / Mathf.PI;
+
+            Debug.Log($"Angle {angle}");
+            
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
         }
 
         
@@ -54,13 +66,19 @@ public class Fox : MonoBehaviour
         if (moveMode)
         {
             rigidbodyComponent.AddForce(moveDirection * Time.fixedDeltaTime * foxSpeed);
-
+            //transform.rotation = newDirection;
+        
             if (rigidbodyComponent.velocity.magnitude > 1)
             {
-                rigidbodyComponent.velocity = rigidbodyComponent.velocity.normalized * 1; 
+                rigidbodyComponent.velocity = rigidbodyComponent.velocity.normalized * 1;
+                //transform.rotation = Quaternion.Euler(rigidbodyComponent.velocity.normalized);
+                //transform.rotation = Quaternion.FromToRotation(Vector3.zero,  rigidbodyComponent.velocity);
             }
-
+        
         }
+        //transform.rotation *= Quaternion.Euler(0, Time.fixedDeltaTime,0);
+        //Debug.Log($"{transform.rotation}");
+        
     }
 
 
@@ -68,6 +86,12 @@ public class Fox : MonoBehaviour
     {
 
         thisRabbit = collision.gameObject.GetComponent<Rabbit>();
+
+        if (thisRabbit is null)
+        {
+            return;
+        }
+        
         Debug.Log("fox collided with rabbit");
         thisRabbit.KillRabbit();
     }
