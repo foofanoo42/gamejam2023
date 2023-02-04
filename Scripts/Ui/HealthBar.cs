e816aa208779d991fe84c11bd96313999185d699
tom <tom.maxwellmans@gmail.com>
2023-02-04 14:29:32 +1300

    Rabbit now has lose condition

--- Scripts/Ui/HealthBar.cs
+++ Scripts/Ui/HealthBar.cs
@@ -38,37 +38,16 @@ namespace Ui
         /// <returns></returns>
         public void UpdatHealth(float healthPercent)
         {
-            //Color currentColor = Color.Lerp(fullHealth, noHealth, healthPercent);
-            //bar.color = currentColor;
-
-            //bar.rectTransform.rect = new Rect( );
-
-            //Rect oldRect = bar.rectTransform.rect;
-            //bar.rectTransform.rect = new Rect(oldRect.x, oldRect.y, healthPercent, oldRect.height);
-
-            Debug.Log($"{healthPercent}");
-            
+            Color currentColor = Color.Lerp(noHealth,fullHealth, healthPercent);
+            bar.color = currentColor;
             SetWidth(healthPercent);
-
         }
 
         private void SetWidth(float width)
         {
             RectTransform rectTransform = bar.rectTransform;
-            
-            Debug.Log($"Update Bar Width {width}");
-            
-            Debug.Log($"{rectTransform.offsetMin.x} {rectTransform.offsetMin.x + _initialOffsetMax.x * width}");
-            
             rectTransform.offsetMax = new Vector2(rectTransform.offsetMin.x + _initialOffsetMax.x * width, _initialOffsetMax.y);
         }
-        
-        // public void SetSize(Vector2 newSize) {
-        //     Vector2 oldSize = trans.rect.size;
-        //     Vector2 deltaSize = newSize - oldSize;
-        //     bar.rectTransform.offsetMin = trans.offsetMin - new Vector2(deltaSize.x * bar.rectTransform.pivot.x, deltaSize.y * bar.rectTransform.pivot.y);
-        //     bar.rectTransform.offsetMax = trans.offsetMax + new Vector2(deltaSize.x * (1f - bar.rectTransform.pivot.x), deltaSize.y * (1f - bar.rectTransform.pivot.y));
-        // }
         
     }
 }
