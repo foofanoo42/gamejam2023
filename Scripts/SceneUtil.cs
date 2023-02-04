a80ea5b29d191e4b82487f1a7bb1bf2d561da7a8
tom <tom.maxwellmans@gmail.com>
2023-02-04 15:19:24 +1300

    Load Helper Methods

--- Scripts/SceneUtil.cs
+++ Scripts/SceneUtil.cs
@@ -1,4 +1,7 @@
-﻿public class SceneUtil
+﻿using UnityEngine.SceneManagement;
+
+
+public static class SceneUtil
 {
 
     public const int SplashScene = 0;
@@ -6,5 +9,11 @@
     public const int PlayScene = 1;
     
     public const int ScoreScene = 2;
+    
+    public static void LoadSplashScreen() => SceneManager.LoadScene(SceneUtil.ScoreScene);
+    
+    public static void LoadPlayScreen() => SceneManager.LoadScene(SceneUtil.PlayScene);
+    
+    public static void LoadScoreScreen() => SceneManager.LoadScene(SceneUtil.ScoreScene);
     
 }
